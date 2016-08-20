7a8562d85db206ddfc842fbf6ddd2d5aa2c673a2
Ryan Nazaretian <ryannazaretian@gmail.com>
2016-08-20 13:17:56 -0400

    Fixed several issues in the GarnetControls file that prevented the robot from going into Teleop.

--- Devices/GarnetSquadron/GarnetControls.py
+++ Devices/GarnetSquadron/GarnetControls.py
@@ -3,6 +3,12 @@ from networktables2.type import BooleanArray, NumberArray
 
 
 class GarnetControls:
+
+    LED_OUTPUTS = 16
+    PWM_OUTPUTS = 11
+    ANALOG_INPUTS = 16
+    SWITCH_INPUTS = 16
+
     SWITCH_OUT = 'Switch'
     ANALOG_OUT = 'Analog'
     PWM_IN = 'PWM'
@@ -15,6 +21,27 @@ class GarnetControls:
         self.led_vals_in = BooleanArray()
         self.ana_vals_out = NumberArray()
         self.pwm_vals_in = NumberArray()
+
+        self.reset_table()
+
+    def reset_table(self, ):
+        self.sw_vals_out.clear()
+        self.led_vals_in.clear()
+        self.ana_vals_out.clear()
+        self.pwm_vals_in.clear()
+
+        self.sw_vals_out.extend([False] * self.SWITCH_INPUTS)
+        self.led_vals_in.extend([False] * self.LED_OUTPUTS)
+        self.ana_vals_out.extend([0] * self.ANALOG_INPUTS)
+        self.pwm_vals_in.extend([0] * self.PWM_OUTPUTS)
+
+        self.nt.putValue(self.SWITCH_OUT, self.sw_vals_out)
+        self.nt.putValue(self.LED_IN, self.led_vals_in)
+        self.nt.putValue(self.ANALOG_OUT, self.ana_vals_out)
+        self.nt.putValue(self.PWM_IN, self.pwm_vals_in)
+
+
+
 
     def getAnalog(self, ch):
         self.nt.getValue(self.ANALOG_OUT, self.ana_vals_out)
